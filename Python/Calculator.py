7cd333ce1cacc981841325ad58f8beb36542fdb3
AstraQuarkX <astraquark1@gmail.com>
2026-07-06 19:21:51 +0530

    Update Calculator.py

--- Python/Calculator.py
+++ Python/Calculator.py
@@ -1,1 +1,18 @@
+def input():
+  while true:
+    equation = print("Enter equation (0 to exit) : ")
+    if equation == "0":
+      break
+    else :
+      print(calculate(equation))
 
+def tokenisation(input):
+  eq = []
+  number = ""
+  for i in range(len(input)) :
+    if input[i].isdigit():
+      number += input[i]
+    elif input[i] in "+-*/%":
+      if input[i+1] == "*" and input[i] == "*":
+        eq.extend([number,"**"])
+      
