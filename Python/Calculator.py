4a61e8d5c529f6496bb8856b1b40f07c485db96a
AstraQuarkX <astraquark1@gmail.com>
2026-07-06 23:20:35 +0530

    Refactor calculator with tokenisation and calculation functions

--- Python/Calculator.py
+++ Python/Calculator.py
@@ -1,18 +1,62 @@
-def input():
-  while true:
-    equation = print("Enter equation (0 to exit) : ")
-    if equation == "0":
-      break
-    else :
-      print(calculate(equation))
+def tokenisation(equation):
+    eq = []
+    num = ""
+    for i in range(0, len(equation)):
+        if equation[i].isdigit() or equation[i] == ".":
+            num += equation[i]
+        elif equation[i] in "+-*/%" and equation[i-1] not in "+-*/%":
+            if equation[i] == "*" and equation[i+1] == "*":
+                operator = "**"
+            elif equation[i] == "/" and equation[i+1] == "/":
+                operator = "//"
+            elif equation[i] in "+-*/%" and equation[i+1] not in "*/":
+                operator = equation[i]
+            if num =="":
+                eq += [operator]
+            else: 
+                eq += [float(num),operator]
+                num = ""
+    eq += [float(num)]
+    if eq[0] == "-":
+        eq = [-eq[1]] + eq[2::]
+    return eq
 
-def tokenisation(input):
-  eq = []
-  number = ""
-  for i in range(len(input)) :
-    if input[i].isdigit():
-      number += input[i]
-    elif input[i] in "+-*/%":
-      if input[i+1] == "*" and input[i] == "*":
-        eq.extend([number,"**"])
-      
+def calculation(e):
+    val = e.copy()
+    while len(val) > 1:
+        if "**" in val:
+            o = val.index("**")
+            val.insert(o-1,val[o-1]**val[o+1])
+            del val[o:o+3]
+        elif "*" in val:
+            o = val.index("*")
+            val.insert(o-1,val[o-1]*val[o+1])
+            del val[o:o+3]
+        elif "/" in val:
+            o = val.index("/")
+            val.insert(o-1,val[o-1]/val[o+1])
+            del val[o:o+3]
+        elif "//" in val:
+            o = val.index("//")
+            val.insert(o-1,val[o-1]//val[o+1])
+            del val[o:o+3]
+        elif "%" in val:
+            o = val.index("%")
+            val.insert(o-1,val[o-1]%val[o+1])
+            del val[o:o+3]
+        elif "-" in val:
+            o = val.index("-")
+            val.insert(o-1,val[o-1]-val[o+1])
+            del val[o:o+3]
+        elif "+" in val:
+            o = val.index("+")
+            val.insert(o-1,val[o-1]+val[o+1])
+            del val[o:o+3]
+    print(f"= {round(val[0],4)}")
+
+while True:
+    a = str(input("Enter equation (0 to exit) : "))
+    if a == "0":
+        break
+    else:
+        calculation(tokenisation(a))
