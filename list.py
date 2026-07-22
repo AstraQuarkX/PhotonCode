170dd4c971b90f6a76644c6e27ee34c9dd02518c
AstraQuarkX <astraquark1@gmail.com>
2026-07-22 22:19:12 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,12 +1,8 @@
 # List :
-brand = ["Quark"]
-brand.append("X")
-brand.insert(0,"Astra")  # list.insert(index,value)
-num = [1,2]
-num.extend([3,4,5])  # num = [1,2,3,4,5]
-chars = ['a','b']
-chars.extend('xy')  # chars = ['a','b','x','y']
-
+brand = ['Quark']
+brand.append('X')
+brand.insert(0,'Astra')  # list.insert(index,value)
+brand.extend([3,4,5])    # brand = ['Astra','Quark','X',3,4,5]
+brand.extend('xy')       # num = ['Astra','Quark','X',3,4,5,'x','y']
 
 del brand[2]  # del list[index]
-
