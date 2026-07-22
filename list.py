4dabf93a749839ec5521f128c5aa1e10337e2aef
AstraQuarkX <astraquark1@gmail.com>
2026-07-22 22:11:59 +0530

    Add list operations examples in list.py

---
+++ list.py
@@ -0,0 +1,12 @@
+# List :
+brand = ["Quark"]
+brand.append("X")
+brand.insert(0,"Astra")  # list.insert(index,value)
+num = [1,2]
+num.extend([3,4,5])  # num = [1,2,3,4,5]
+chars = ['a','b']
+chars.extend('xy')  # chars = ['a','b','x','y']
+
+
+del brand[2]  # del list[index]
+
