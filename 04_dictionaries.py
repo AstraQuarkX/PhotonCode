b983d1320b77eba856bb4e276021ddfb1dcd0c0a
AstraQuarkX <astraquark1@gmail.com>
2026-07-24 21:49:18 +0530

    Update 04_dictionaries.py

--- 04_dictionaries.py
+++ 04_dictionaries.py
@@ -13,4 +13,36 @@ for key, value in color.items():
 for key in color.keys():
   print(f"Color : {key}")
 for value in color.values():
-  print("Value : %d",value)
+  print("Value : %d" % (value))
+# Looping through the Keys in Order : 
+for key in sorted(color.keys()):
+  print(f"Color : {key}")
+# For Getting only Unique Values (Duplicate Values are not Repeated) :
+for value in set(color.values()):
+  print("Value : %d" % (value))
+
+# List of Dictionaries : 
+color = {'black' : 1, 
+        'blue' : 2, 
+        'grey' : 3
+        }
+sub = {'phy' : 1, 
+        'math' : 2, 
+        'chem' : 3
+        }
+l = [color,sub]
+print(l[0]['blue'])  #>>> 2
+
+# Nested Dictionaries :
+users = {
+         'HB': {
+                  'first': 'Homi',
+                  'last': 'Bhabha',
+         },
+         'VS': {
+                  'first': 'Vikram',
+                  'last': 'Sarabhai',
+         }
+}
+print(users['HB'])
+
