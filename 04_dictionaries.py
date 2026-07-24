c7f59507f03d7b067268974d116292127bbf10c9
AstraQuarkX <astraquark1@gmail.com>
2026-07-24 21:35:09 +0530

    Update 04_dictionaries.py

--- 04_dictionaries.py
+++ 04_dictionaries.py
@@ -1,1 +1,16 @@
+# Creating a Dictionary :
+color = {'black' : 1, 
+         'blue' : 2, 
+         'grey' : 3
+        }
+color['white'] = 4  # Adding a key-value pair
+color['grey'] = 5  # Changing a value
+del color['blue']  # Removing a key value pair
 
+# Accessing Key-Value Pairs from Dictionary using Loop :
+for key, value in color.items():
+  print("{} : {}".format(key,value))
+for key in color.keys():
+  print(f"Color : {key}")
+for value in color.values():
+  print("Value : %d",value)
